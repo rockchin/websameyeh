5ed91bda37858e489ac3b6d97c445cb19211f9c9
rockchin <hoang_duyen2367@yahoo.com.vn>
2015-07-19 22:38:13 +0700

    update slider for home page

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,28 +1,22 @@
-$(document).ready(function(){
-	$('.center').slick({
-		centerMode: true,
-		centerPadding: 0,
-		slidesToShow: 3,		
-		variableWidth: true,
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false,
-				centerMode: true,
-				centerPadding: 0,
-				slidesToShow: 3
-			}
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				arrows: false,
-				centerMode: true,
-				centerPadding: 0,
-				slidesToShow: 3
-			}
-		}
-		]
+$(document).ready(function() {
+
+	$('.slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		asNavFor: '.slider-nav'
 	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		dots: false,
+		centerMode: true,
+		focusOnSelect: true,
+		arrows: false,
+		variableWidth:true
+	});
+	
 });
+	 
